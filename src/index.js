db680c222228b7a6319a0afa37e40e0955b0f61b
KeMull <hexinyu@lpcollege.com>
2021-01-30 12:38:07 +0800

    <bug修复>:
    # 影响范围:
    
    # 我修改的原因:

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
  * @Author: KeMull
  * @Date: 2021-01-27 10:54:14
  * @LastEditors: KeMull
- * @LastEditTime: 2021-01-28 17:42:17
+ * @LastEditTime: 2021-01-30 11:44:28
  */
 // 入口文件
 
@@ -13,3 +13,30 @@ import './assets/iconfont/iconfont.css'
 const handleAdd = (x, y = 10) => {
 	return x + y
 }
+console.log(
+	'%c 🥟 handleAdd: ',
+	'font-size:20px;background-color: #FCA650;color:#fff;',
+	handleAdd(1, 3)
+)
+const handleClick = () => {
+	console.log('3132131')
+}
+const handleClick1 = () => {
+	console.log('3132131')
+}
+const handleClick3 = () => {
+	console.log('3132131')
+}
+const handleClick323 = () => {
+	console.log('3132131')
+}
+const handleClick32322 = () => {
+	console.log('3132131')
+}
+const handleClick32322321321 = () => {
+	console.log('3132131')
+}
+const handleClick3232313212322321321 = () => {
+	console.log('3132131')
+}
+const a = 132132131
--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,13 @@
  * @Author: KeMull
  * @Date: 2021-01-27 11:21:48
  * @LastEditors: KeMull
- * @LastEditTime: 2021-01-28 18:03:43
+ * @LastEditTime: 2021-01-30 11:58:45
  */
 const path = require('path')
+const webpack = require('webpack')
+const { HotModuleReplacementPlugin } = webpack
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const UglifyjsWebpackPlugin = require('uglifyjs-webpack-plugin')
 // MiniCssExtractPlugin: 把js中import导入的样式文件，单独打包成一个css文件，结合html-webpack-plugin，以link的形式插入到html文件中。
 const MiniCssExtractPlugin = require('mini-css-extract-plugin') // 不能和style-loader一起使用  会出错
 const { CleanWebpackPlugin } = require('clean-webpack-plugin') // 不能和style-loader一起使用  会出错
@@ -48,12 +51,12 @@ module.exports = {
 			},
 			{
 				test: /\.html$/,
-				// html-loader引入的image 使用的是commonJs的规则
+				// 处理 html-loader引入的image 使用的是commonJs的规则
 				loader: 'html-loader',
 			},
 			{
 				// 排除css/js/html/less资源  以外的资源使用file-loader去处理
-				exclude: /\.(css|js|html|less)$/,
+				exclude: /\.(css|js|html|less|jpg|png|gif)$/,
 				loader: 'file-loader',
 				options: {
 					name: `static/[name]_[hash:8].[ext]`,
@@ -63,17 +66,58 @@ module.exports = {
 	},
 
 	plugins: [
-		new CleanWebpackPlugin(),
+		// new HotModuleReplacementPlugin(), // 热更新插件
+
+		new MiniCssExtractPlugin({
+			filename: 'css/[name].[hash].css',
+			chunkFilename: 'css/[id].[hash].css',
+		}),
+		// new webpack.ProgressPlugin(),
+		new CleanWebpackPlugin({
+			// root: path.resolve(__dirname, '..'),
+			dry: false,
+			verbose: true,
+		}),
 		new HtmlWebpackPlugin({
 			// 复制html 并自动打包输出所有的(js/css)资源
 			// 格式化模板
 			template: './src/index.html',
 		}),
-		new MiniCssExtractPlugin({
-			filename: 'css/[name].[hash].css',
-			chunkFilename: 'css/[id].[hash].css',
-		}),
+		// new UglifyjsWebpackPlugin(),
 	],
+	optimization: {
+		minimizer: [
+			// new UglifyjsWebpackPlugin({
+			// 	uglifyOptions: {
+			//    parallel:true, //并行化可以显着加快构建速度，因此强烈建议使用
+			// 		output: {
+			// 			comments: false,
+			// 		},
+			// 		compress: {
+			// 			warnings: false,
+			// 			drop_debugger: true,
+			// 			drop_console: true,
+			// 			pure_funcs: ['console.log'], //移除console
+			// 		},
+			// 	},
+			// }),
+		],
+	},
+	// 开发服务器 (自动编译,自动打开浏览器,自动刷新浏览器)
+	// 特点: 只会在内存中编译打包,不会有任何输出
+	devServer: {
+		contentBase: resolve(__dirname, 'dist'), //运行的文件目录
+		compress: true, //优化压缩
+		port: 8741,
+		open: true,
+		define: {
+			'process.env': {
+				MY_NODE_ENV: process.env.MY_NODE_ENV,
+				VERSION_CODE: process.env.VERSION_CODE,
+			},
+		},
+	},
+	// 环境
 	mode: 'development',
 	// mode: 'production',
 	// define: {
