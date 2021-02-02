2c111c23b7dc1a435cb4eb20cd3d855b25ca08f2
KeMull <hexinyu@lpcollege.com>
2021-02-02 19:20:38 +0800

    <新功能>:
    # 影响范围:
    
    # 我修改的原因:

---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+/*
+ * @Author: KeMull
+ * @Date: 2021-01-30 11:50:31
+ * @LastEditors: KeMull
+ * @LastEditTime: 2021-02-02 10:25:21
+ */
+import ReactDOM from 'react-dom'
+import React from 'react'
+
+import Home from './pages/Home/index'
+ReactDOM.render(<Home />, document.getElementById('root'))
